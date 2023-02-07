ac3a65473e72e7f24d57d51ff0d9f8dd0b464eae
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-02-07 11:01:00 +0300

    404 error page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ErrorComponent } from './component/error/error.component';
 import { PasswordProjectComponent } from './component/password-project/password-project.component';
 import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'password-project', component: PasswordProjectComponent },
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { PasswordProjectComponent } from './component/password-project/password-project.component';
+import { ErrorComponent } from './component/error/error.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PasswordProjectComponent],
+  declarations: [AppComponent, HomeComponent, PasswordProjectComponent, ErrorComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
