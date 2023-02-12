bb4478ab3dcff097ef15193fa4c9bfab7cfad8a1
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-02-12 21:14:19 +0300

    arrangement

--- src/app/component/routing-state-management/rst-tables/rst-tables.component.ts
+++ src/app/component/routing-state-management/rst-tables/rst-tables.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rst-tables',
   templateUrl: './rst-tables.component.html',
-  styleUrls: ['./rst-tables.component.css']
+  styleUrls: ['./rst-tables.component.css'],
 })
 export class RstTablesComponent implements OnInit {
+  datas = [
+    { name: 'Jhon', age: 23, job: 'Computer Engineer', employed: 'No' },
+    { name: 'Jack', age: 42, job: 'Software Engineer', employed: 'No' },
+    { name: 'Will', age: 38, job: 'Industrial Engineer', employed: 'Yes' },
+  ];
+  headers = [
+    { key: 'name', label: 'Fullname' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Is he/she satisfied with his/her salary?' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
