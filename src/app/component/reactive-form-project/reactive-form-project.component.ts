1fba1991f8bbde52c5fc55b943b25a965f28f444
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-02-15 11:44:49 +0300

    arrangement

--- src/app/component/reactive-form-project/reactive-form-project.component.ts
+++ src/app/component/reactive-form-project/reactive-form-project.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-project',
   templateUrl: './reactive-form-project.component.html',
-  styleUrls: ['./reactive-form-project.component.css']
+  styleUrls: ['./reactive-form-project.component.css'],
 })
 export class ReactiveFormProjectComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
