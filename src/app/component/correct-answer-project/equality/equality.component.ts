eb82c70e75843d09fcd89c30c232c56735aab3a7
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-02-25 00:41:41 +0300

    bootstrap edits left

--- src/app/component/correct-answer-project/equality/equality.component.ts
+++ src/app/component/correct-answer-project/equality/equality.component.ts
@@ -1,6 +1,8 @@
+import { startWith } from 'rxjs/operators';
 import { EqualityValidators } from './equality-validators';
 import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { delay, filter } from 'rxjs';
 
 @Component({
   selector: 'app-equality',
@@ -8,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equality.component.css'],
 })
 export class EqualityComponent implements OnInit {
+  current = '';
+  totalSeconds = 0;
+  averageSeconds = 0;
+  solved = 0;
   mathForm = new FormGroup(
     {
       firstNumber: new FormControl(this.generateNumber()),
@@ -28,16 +34,28 @@ export class EqualityComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.mathForm.statusChanges.subscribe((value) => {
-      if (value === 'INVALID') {
-        return;
-      }
-      this.mathForm.setValue({
-        firstNumber: this.generateNumber(),
-        secondNumber: this.generateNumber(),
-        answer: '',
+    const startTime = new Date();
+
+    // VALID veya INVALID değerlerini döndürür.
+    // console.log( this.mathForm.statusChanges);
+
+    this.mathForm.statusChanges
+      .pipe(
+        filter((value) => value === 'VALID'),
+        delay(1000)
+      )
+      .subscribe((value) => {
+        this.solved++;
+        this.totalSeconds = (new Date().getTime() - startTime.getTime()) / 1000;
+        this.averageSeconds =
+          (new Date().getTime() - startTime.getTime()) / this.solved / 1000;
+        // setValue tüm değerleri güncellerken patchValue girilen değerleri update eder.
+        this.mathForm.setValue({
+          firstNumber: this.generateNumber(),
+          secondNumber: this.generateNumber(),
+          answer: '',
+        });
       });
-    });
   }
 
   generateNumber() {
